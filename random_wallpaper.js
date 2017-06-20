703b62d66c6a09cf677b27f6925abea07e93e522
Mohan <linchawk@users.noreply.github.com>
2017-06-20 15:47:17 +0530

    Promisifying the core functions

--- random_wallpaper.js
+++ random_wallpaper.js
@@ -1,46 +1,71 @@
 const https = require('https');
 const fs = require('fs');
-const wallpaper = require('wallpaper');
-const wallpaperChangeFrequency = 7;
-var wallpaperUrl = 'https://source.unsplash.com/collection/1227/1600x900';
+const wallpaperChanger = require('wallpaper');
 
-function downloadNewRandomImage(url) {
-    https.get(url, function(response) {
-        if(response.statusCode == '302') {
-            downloadNewRandomImage(response.headers['location']);
-        }
-        else {
-        	var file = fs.createWriteStream('wall.jpg');
-            response.pipe(file);
-            response.on('end', function() {
-                console.log('Wallpaper downloaded.');
-                wallpaper.set('wall.jpg').then(function() {
-                    console.log('Wallpaper changed.');
-                });
-            })
-        }
-    });
+function Wallpaper() {
+
 }
 
-function getDateDiffInDays(date1, date2) {
-	var diffInTime = Math.abs(date2.getTime() - date1.getTime());	
-	var diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));	
-	return diffInDays;
-}
-
-function main() {
-	var stats = fs.statSync('wall.jpg');
+Wallpaper.prototype.shouldDownload = function(fileName, wallpaperChangeFrequency) {
+	var stats = fs.statSync(fileName);
 	var fileLastModifiedDate = new Date(stats.mtime);
 	console.log('Wallpaper last modified on ' + fileLastModifiedDate);	
 	var elapsedDays = getDateDiffInDays(fileLastModifiedDate, new Date());	
 
 	if(elapsedDays >= wallpaperChangeFrequency) {
-		console.log("Downloading new random wallpaper from Unsplash.com");
-		downloadNewRandomImage(wallpaperUrl)
+		console.log("Should download new wallpaper");
+		return true;
 	}
 	else {
-		console.log("Not going to change the wallpaper");
+		console.log("No need to download new wallpaper");
+		return false;
 	}
+	
+	function getDateDiffInDays(date1, date2) {
+		var diffInTime = Math.abs(date2.getTime() - date1.getTime());	
+		var diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));	
+		return diffInDays;
+	}
+};
+
+Wallpaper.prototype.download = function(url) {
+	var download1 = function(url) {
+		return new Promise(function(resolve, reject) {
+			https.get(url, function(response) {
+				resolve(response);
+			});
+		});
+	};
+
+	return new Promise(function(resolve, reject) {
+		var downloadPromise = download1(url).then(function(response) {
+			if(response.statusCode == '302') {
+				return download1(response.headers['location']);
+			}			
+		}).then(function(response) {
+			var file = fs.createWriteStream('wall.jpg');
+            response.pipe(file);
+            response.on('end', function() {
+                resolve('Wallpaper downloaded.');
+            });
+		});
+	});
+};
+
+
+function main() {
+	var fileName = 'wall.jpg';
+	var changeFrequency = 7;
+	var downloadUrl = 'https://source.unsplash.com/collection/1227/1600x900';
+	var wallpaper = new Wallpaper();
+	if(wallpaper.shouldDownload(fileName, changeFrequency)) {
+		wallpaper.download(downloadUrl).then(function(msg) {
+			console.log(msg);
+			return wallpaperChanger.set(fileName);
+		}).then(function() {
+			console.log('Wallpaper changed.');
+		});
+	}		
 }
 
 main();
